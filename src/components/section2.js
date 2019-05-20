8e79bf7e1d5bf70cb23e0156c85d6a5935b93636
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-05-20 17:11:45 +0600

    Horoscope section of home page is done

--- src/components/section2.js
+++ src/components/section2.js
@@ -6,8 +6,7 @@ class Section2 extends Component {
       return (
         <div className="section">
           <Title />
-          <Subsection1 />
-          <Subsection2 />
+          <Subsection />          
           <Button />
         </div> 
       )
@@ -15,38 +14,35 @@ class Section2 extends Component {
   }
   class Title extends Component {  
     render(){
-      return (
-        <div className="title">
-          <h2 className="display2">Почему важно знать свой гороскоп?</h2>
-          <h3 className="display3">Потому что, это карта жизни, на которой нанесены все ваши взлеты и падения, легкие и сложные маршруты, преграды и обходные пути</h3>
-        </div> 
+      return (        
+        <h2 className="display2">НАТАЛЬНАЯ КАРТА</h2>        
       )
     }
   }
-  class Subsection1 extends Component {  
+  class Subsection extends Component {  
     render(){
       return (
-        <div className="/">
-          
+        <div className="subsection">
+          <div className="subsection subsection__natal">
+            <h3 className="display3">Зачем мне нужен гороскоп?</h3>
+            <span className="subsection__natal-text">Натальная карта человека - это его карта жизни. В ней отражены взлеты и падения, легкие и сложные маршруты, преграды и обходные пути.
+            Конечно, вы можете идти наугад и обретать богатый опыт собственных ошибок. Но мне кажется, есть в вашей жизни и более приятный маршрут.
+            И астрология будет вам надежным другом. Помните, индивидуальный гороскоп — это надежный инструмент.
+            </span>
+          </div>
+          <div className="subsection subsection__natal">
+            <h3 className="display3">Что такое настоящий гороскоп?</h3>
+            <span className="subsection__natal-text">Настоящий гороскоп — это точная карта звездного неба, составленная на момент рождения конкретного человека. При этом астролог учитывает место рождения и точное время рождения.  В астрологической (натальной) карте учитывается положение 9 планет, Солнца, Луны и астероидов. Конечно, сейчас карта составляется с помощью компьютера. А вот интерпретация гороскопа –  сложное и длительное занятие. Иногда я трачу 4-5 часов, чтобы сопоставить множество факторов, найти особые знаки. И тут компьютер — не помощник. Большая стопка толстых старых справочников и опыт — вот эффективные инструменты профессионального астролога.</span>
+          </div>            
         </div> 
       )
     }
   }
-  class Subsection2 extends Component {  
-    render(){
-      return (
-        <div className="/">
-          
-        </div> 
-      )
-    }
-  }
+
   class Button extends Component {  
     render(){
       return (
-        <div className="/">
-          
-        </div> 
+        <button className="btn btn__horo btn__horo_active" type="submit">Рассчитать карту</button>          
       )
     }
   }
