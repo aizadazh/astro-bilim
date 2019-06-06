77e180145f312cebc1e183942976336dc7f2f571
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-06-06 10:06:35 +0600

    article section is done

---
+++ src/components/Article.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import "./article.scss";
+import "./base.scss";
+
+class Article extends PureComponent {  
+
+/*
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.isOpen !== nextState.isOpen
+    }
+*/
+
+    componentWillMount() {
+        console.log('---', 'mounting')
+    }
+
+/*
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+            isOpen: nextProps.defaultOpen
+        })
+    }
+*/
+
+    componentWillUpdate() {
+        console.log('---', 'will update')
+    }
+
+    render() {
+        const {article, isOpen, onButtonClick} = this.props
+        const style = {width: '100%'}
+        const body = isOpen && <section className="card-text">{article.text}</section>
+        return (
+            <div className="container" style = {style}>
+                <div className="card-header">
+                    <h2>
+                        {article.title}
+                        <button onClick={onButtonClick} className="close-button">
+                            {isOpen ? 'Закрыть' : 'Подробнее'}
+                        </button>
+                    </h2>
+                </div>
+                <div className="article-body">
+                    <h6 className="article-date">
+                        creation date: {(new Date(article.date)).toDateString()}
+                    </h6>
+                    {body}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+
+export default Article
--- src/pages/article.js
+++ src/pages/article.js
@@ -1,14 +1,45 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { MainLayout } from "../components";
+import ArticleList from '../components/ArticleList';
+import articles from '../components/fixture';
 
-class ArticlePage extends Component{
+const divStyle={  
+  margin: '0 auto',
+  width: '100%',
+  maxWidth: '1200px',  
+  height:'20vh',
+  marginTop: '0.3rem',
+  background: '#060620',
+};
+const hStyle={  
+  width: '100%',
+  maxWidth: '1200px',  
+  textAlign: 'center',
+  paddingTop: '2.3rem',
+  color: 'white',
+};
+class ArticlePage extends PureComponent{
+  state = {
+    reverted: false
+    
+}
+
   render(){
     return (
-      <MainLayout>    
-        
+      <MainLayout>
+        <div style={divStyle}>
+          <h1 style={hStyle}>
+            Статьи для обсуждения            
+          </h1>
+        </div>
+        <ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>        
       </MainLayout>       
     )
   }
 }
+
+
+
+
 
 export default ArticlePage;
